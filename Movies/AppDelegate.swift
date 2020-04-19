566be7cf51236ae17b896d3b04547f9ed34613f7
Oğuzhan Karakuş <oguzhan.karakus@loodos.com>
2020-04-19 05:19:30 +0300

    add root vc

--- Movies/AppDelegate.swift
+++ Movies/AppDelegate.swift
@@ -15,10 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window = UIWindow(frame:UIScreen.main.bounds)
-        let rootVC = UINavigationController(rootViewController: ViewController())
-        window?.rootViewController = rootVC
-        window?.makeKeyAndVisible()
+        window = UIWindow(rootViewController: MovieListViewController())
         return true
     }
 
